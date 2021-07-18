4d9cbff4cdbebe02f850cf2f8b0182a09b17df9a
vishal <vishaljain1211>
2021-07-18 16:53:10 +0530

    first

--- manage.py
+++ manage.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 
-def main():
+def main(para):
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'onlinequiz.settings')
     try:
         from django.core.management import execute_from_command_line
@@ -14,7 +14,7 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
-    execute_from_command_line(sys.argv)
+    execute_from_command_line(para)
 
 main(['makemigrations'])
 main(['migrate'])
